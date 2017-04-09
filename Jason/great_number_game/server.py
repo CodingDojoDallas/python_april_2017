b0357c770e5f96426136c4396d300f4fd82983af
Jason <rhuidean06@gmail.com>
2017-04-09 00:05:07 -0500

    Great number game. Properly working

--- Jason/great_number_game/server.py
+++ Jason/great_number_game/server.py
@@ -3,34 +3,6 @@ import random
 
 app=Flask(__name__)
 
-### Case 1: using a global variable for setting the random.randint number
-# # seed_number=1
-# great_num=random.randint(1,100)
-# print great_num
-
-# @app.route('/')
-# def index():
-# 	print great_num
-# 	return render_template('index.html')
-
-# @app.route('/submit')
-# def submit():
-# 	num = request.form[number]
-# 	if num > great_num:
-# 		session['status'] = "above"
-# 	elif num < great_num:
-# 		session['status'] = "below"
-# 	else:
-# 		session['status'] = "on the money"
-# 		# seed_number+=1
-# 		great_num=randint(1,100)
-
-# 	return redirect('/',status=status)
-
-
-
-### Case 2:using session for setting random number and assigning input numbers.
-
 app.secret_key ='thisissecret'
 
 @app.route('/')
@@ -43,13 +15,14 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-	session['number'] = request.form['number']
+	session['number'] = int(request.form['number'])
+	print session['number'] > session['randomint'] 
 	print type(session['number'])
 	print session['number']
 	if session['number'] > session['randomint']:
 		session['status'] = "above"
 	elif session['number'] < session['randomint']:
-		session['number'] = "below"
+		session['status'] = "below"
 	else:
 		session['status'] = "on the money"
 	print session['status']
