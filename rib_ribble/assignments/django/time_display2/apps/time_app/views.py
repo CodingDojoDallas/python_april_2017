48e5be7680613ff51d7cd977635e7db39c6db878
Rib <ribribble@gmail.com>
2017-04-08 23:05:13 -0500

    added time_display assignment

---
+++ rib_ribble/assignments/django/time_display2/apps/time_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+import datetime
+
+def index(request):
+	context = {
+		'time': datetime.datetime.now()
+	}
+	return render(request,'time_app/index.html',context)
