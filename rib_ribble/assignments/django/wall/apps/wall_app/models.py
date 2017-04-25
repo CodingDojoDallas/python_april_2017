f6b45c9e0383e7eba8e4d26630f1eff318048c60
Rib <ribribble@gmail.com>
2017-04-25 15:06:03 -0500

    added wall assignment utilizing relationships with ForeignKeys

---
+++ rib_ribble/assignments/django/wall/apps/wall_app/models.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class User( models. Model):
+	first_name= models.CharField(max_length=255)
+	last_name= models.CharField(max_length=255)
+	email = models.CharField(max_length=255)
+	password = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+class Post( models. Model):
+	message= models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	user_id= models.ForeignKey('User')
+
+
+class Comment( models. Model):
+	message= models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	user_id= models.ForeignKey('User')
+	post_id=models.ForeignKey('Post')
+
+
