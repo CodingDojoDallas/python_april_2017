d62ffcbe1f0636392f7cb2fb5e57876d8730659b
timber <mister.modistette@gmail.com>
2017-04-30 13:28:07 -0500

    books assignment

--- timber_modisette/pythonDjango/books/main/settings.py
+++ timber_modisette/pythonDjango/books/main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qc#s(^x@!2fup-0u#yaxo3=euk!s-ejg-mny4syx#f=xe2#fuq'
+SECRET_KEY = '$elp*vj)2!b%+44kds5vdr)s92u2%i!(qazy#buaqu&i-ao*q0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
